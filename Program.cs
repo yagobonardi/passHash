902a3f092e9259d518c92479897dbadc9876ca40
yagobonardi <yagobonardi@gmail.com>
2024-12-24 15:03:47 -0300

    teste comparacao hash

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("digite uma senha");
+
+var password = Console.ReadLine();
+
+var hashedPassword = BCrypt.Net.BCrypt.HashPassword(password);
+
+Console.WriteLine("digite sua senha novamente");
+
+password = Console.ReadLine();
+
+var isVerify = BCrypt.Net.BCrypt.Verify(password, hashedPassword);
+
+Console.WriteLine(isVerify);
